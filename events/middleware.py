d5c3e3f29bc925e9e6dd518e2a604ef06cf9db2e
quynh <tqquynh@gmail.com>
2016-06-20 09:46:49 +0800

    reformat file

--- events/middleware.py
+++ events/middleware.py
@@ -1,4 +1,4 @@
-#define the custom middleware for authentication
+# define the custom middleware for authentication
 import json
 
 from django.http import HttpResponse
@@ -7,9 +7,10 @@ from events.models import User
 from events import constants
 from datetime import datetime
 
+
 class AuthenticationMiddleware(object):
-    def process_request(self,request):
-        #if login or signup --> pass through the url
+    def process_request(self, request):
+        # if login or signup --> pass through the url
         if 'signup' in request.get_full_path() \
                 or 'login' in request.get_full_path():
             return None
@@ -21,11 +22,12 @@ class AuthenticationMiddleware(object):
             if 'token' in request.POST:
                 token = request.POST['token']
         if (token == None):
-            return HttpResponse(json.dumps({'error':constants.TOKEN_NOT_FOUND_OR_EXPIRED}), status=403, content_type="application/json")
+            return HttpResponse(json.dumps({'error': constants.TOKEN_NOT_FOUND_OR_EXPIRED}), status=403,
+                                content_type="application/json")
         else:
             users = User.objects.filter(token=token)
             if not users.exists():
-                return HttpResponse(json.dumps({'error':constants.TOKEN_NOT_FOUND_OR_EXPIRED}), status=403,
+                return HttpResponse(json.dumps({'error': constants.TOKEN_NOT_FOUND_OR_EXPIRED}), status=403,
                                     content_type="application/json")
             else:
                 # if token expired, not pass
@@ -35,4 +37,3 @@ class AuthenticationMiddleware(object):
 
     def process_template_response(self, request, response):
         return response
-
