c569f8b4e413a7816ba782d8b4e2eb06d29d0842
quynh <tqquynh@gmail.com>
2016-06-20 09:45:47 +0800

    add logic for middleware

--- events/middleware.py
+++ events/middleware.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 from events.models import User
 from events import constants
-
+from datetime import datetime
 
 class AuthenticationMiddleware(object):
     def process_request(self,request):
@@ -27,8 +27,12 @@ class AuthenticationMiddleware(object):
             if not users.exists():
                 return HttpResponse(json.dumps({'error':constants.TOKEN_NOT_FOUND_OR_EXPIRED}), status=403,
                                     content_type="application/json")
+            else:
+                # if token expired, not pass
+                if users[0].tokenExpiredOn < datetime.utcnow():
+                    return HttpResponse(json.dumps({'error': constants.TOKEN_NOT_FOUND_OR_EXPIRED}), status=403,
+                                        content_type="application/json")
 
-
-def process_template_response(self, request, response):
-    return response
+    def process_template_response(self, request, response):
+        return response
 
