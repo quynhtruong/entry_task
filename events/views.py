30fd095f0dfb66e272b2b37e879d2865a768a371
quynh <tqquynh@gmail.com>
2016-06-20 18:01:50 +0800

    reformat file

--- events/views.py
+++ events/views.py
@@ -8,11 +8,13 @@ from models import Like
 import time
 import json
 import os
-from datetime import  datetime
+from datetime import datetime
 from django.utils import timezone
 from events import constants
 
 queryTemplate = ''
+
+
 # Create your views here.
 def list_all(request):
     # capture all parameters
@@ -34,11 +36,11 @@ def list_all(request):
         channelId = request.GET['channelId']
     if 'startDate' in request.GET:
         startDate = datetime.fromtimestamp((float)(request.GET['startDate'].__str__())).strftime('%Y-%m-%d %H:%M:%S')
-    if 'endDate' in  request.GET:
+    if 'endDate' in request.GET:
         endDate = datetime.fromtimestamp((float)(request.GET['endDate'].__str__())).strftime('%Y-%m-%d %H:%M:%S')
 
     # build query
-    #read query template from file
+    # read query template from file
     global queryTemplate
     if queryTemplate == '':
         dirPath = os.path.dirname(__file__) + '/raw_query/list_events.sql'
@@ -47,13 +49,13 @@ def list_all(request):
 
     query = queryTemplate
     if channelId is not None:
-        query = query + ' and ch.id = '+channelId
+        query = query + ' and ch.id = ' + channelId
     if startDate is not None:
-        query = query+ ' and e.start_date >= '+ "'"+startDate+"'"
+        query = query + ' and e.start_date >= ' + "'" + startDate + "'"
     if endDate is not None:
-        query = query+ ' and e.end_date <= '+ "'"+endDate+"'"
-    query = query+' GROUP BY e.id LIMIT '+str(pageSize)+' OFFSET '+ str(pageIndex*pageSize)
-    #execute query
+        query = query + ' and e.end_date <= ' + "'" + endDate + "'"
+    query = query + ' GROUP BY e.id LIMIT ' + str(pageSize) + ' OFFSET ' + str(pageIndex * pageSize)
+    # execute query
     eventList = Event.objects.raw(query)
     now = timezone.make_aware(datetime.now(), timezone.get_current_timezone())
 
@@ -75,7 +77,7 @@ def list_all(request):
         responseData['creatorName'] = event.owner_name
         responseData['creatorAvatarId'] = event.owner_avatar_id
         if event.id in listIdList:
-            responseData['isLiked']  = True
+            responseData['isLiked'] = True
         else:
             responseData['isLiked'] = False
         if event.startDate >= now:
@@ -88,6 +90,7 @@ def list_all(request):
         responseDataList.append(responseData)
     return HttpResponse(json.dumps(responseDataList), status=200, content_type="application/json")
 
+
 def listParticipant(request):
     id = request.GET['id']
     event = None
@@ -95,10 +98,11 @@ def listParticipant(request):
     try:
         event = Event.objects.get(id=id)
     except Event.DoesNotExist:
-        return HttpResponse(json.dumps({'error':constants.EVENT_NOT_FOUND}), status=500, content_type="application/json")
+        return HttpResponse(json.dumps({'error': constants.EVENT_NOT_FOUND}), status=500,
+                            content_type="application/json")
 
     for participant in Participant.objects.filter(event=event):
-        user  = participant.user
+        user = participant.user
         responseData = {}
         responseData['email'] = user.email
         responseData['password'] = user.password
@@ -108,6 +112,3 @@ def listParticipant(request):
         responseDataList.append(responseData)
 
     return HttpResponse(json.dumps(responseDataList), status=200, content_type="application/json")
-
-
-
